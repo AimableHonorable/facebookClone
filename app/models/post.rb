a91fb83fd20325a2499441d638c08fccbfa97bcd
Aimable <aimatwiri@gmail.com>
2019-09-11 10:40:57 +0200

    validation of model to prohibit the user from posting null contents

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
   mount_uploader :images, ImageUploader
+
+  validate :any_present?
+
+  def any_present?
+    if %w(texts images).all?{|attr| self[attr].blank?}
+      errors.add :base, "please post at least one field"
+    end
+  end
+
 end
